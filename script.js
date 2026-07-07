627eb9367367658fc68736c117f7a4d413d58b1b
Payal <pwagh642018@gmail.com>
2026-07-07 19:14:13 +0530

    added dark mode

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const totalTasks = document.getElementById("totalTasks");
 const completedTasks = document.getElementById("completedTasks");
 const pendingTasks = document.getElementById("pendingTasks");
 
+const themeBtn = document.getElementById("themeBtn");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    themeBtn.textContent = "☀️ Light Mode";
+}
+
  
 
 // Load tasks from localStorage
@@ -28,6 +35,24 @@ function renderTask() {
         taskList.innerHTML = "<li>📋 No tasks available.</li>";
 
     }
+
+    themeBtn.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+
+        localStorage.setItem("theme", "dark");
+        themeBtn.textContent = "☀️ Light Mode";
+
+    } else {
+
+        localStorage.setItem("theme", "light");
+        themeBtn.textContent = "🌙 Dark Mode";
+
+    }
+
+});
 
 
     tasks.forEach(function (task, index) {
